7a57ff80094debe9cbabe5d28f04b20eac0033db
unknown <jagadyudhaawali@gmail.com>
2021-09-17 23:47:01 +0700

    fix: button darkmode

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,27 +1,37 @@
 import React from "react";
-import DarkMode from "../darkmode";
 import { IoMoon, IoSunny } from "react-icons/io5";
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
 
 function navbar() {
-  const [colorTheme, setTheme] = DarkMode();
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
   return (
     <div className="sticky top-0 z-50 dark:bg-mydark bg-mylight backdrop-filter backdrop-blur-lg dark:bg-opacity-60 bg-opacity-50">
       <nav className="flex max-w-3xl mx-auto text-center sm:text-right py-4 items-center justify-between px-5">
-        {colorTheme === "light" ? (
-          <button
-            onClick={() => setTheme("light")}
-            className="bg-white bg-opacity-10 rounded-md font-normal text-md sm:text-lg p-2 sm:mr-2 mr-0"
-          >
-            <IoMoon className="text-white text-lg sm:text-2xl" />
-          </button>
-        ) : (
-          <button
-            onClick={() => setTheme("dark")}
-            className="bg-black bg-opacity-10 rounded-md font-normal text-md sm:text-lg p-2 sm:mr-2 mr-0"
-          >
-            <IoSunny className="text-black text-lg sm:text-2xl" />
-          </button>
-        )}
+        <button
+          type="button"
+          className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
+          onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+        >
+          {mounted && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              className="w-4 h-4 text-mybg dark:text-gray-200"
+            >
+              {resolvedTheme === "dark" ? (
+                <IoSunny className="text-white text-lg sm:text-2xl" />
+              ) : (
+                <IoMoon className="text-black text-lg sm:text-2xl" />
+              )}
+            </svg>
+          )}
+        </button>
         <div>
           <a
             href="/"
--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,16 @@
 import "tailwindcss/tailwind.css";
-import MyLayout from "../components/layout/layout";
+import Layout from "../components/layout/layout";
+import { ThemeProvider } from "next-themes";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MyLayout>
-      <div className="max-w-2xl px-8 mx-auto my-8 sm:my-14">
-        <Component {...pageProps} />
-      </div>
-    </MyLayout>
+    <ThemeProvider attribute="class">
+      <Layout>
+        <div className="max-w-2xl px-8 mx-auto my-8 sm:my-14">
+          <Component {...pageProps} />
+        </div>
+      </Layout>
+    </ThemeProvider>
   );
 }
 
