ec11c4681b700988b4fc8679822941cb70bf409d
unknown <jagadyudhaawali@gmail.com>
2021-10-16 23:16:27 +0700

    feat: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,25 @@ import { DefaultSeo } from 'next-seo';
 import { cardOpenGraph, cardTwitter } from '../lib/seo';
 import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }) {
   const router = useRouter();
+
+  const handleRouteChange = (url) => {
+    window.gtag('config', process.env.NEXT_PUBLIC_ANALYTICS, {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <ThemeProvider attribute='class'>
       <DefaultSeo
@@ -17,6 +33,23 @@ export default function MyApp({ Component, pageProps }) {
         openGraph={cardOpenGraph}
         twitter={cardTwitter}
       />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_ANALYTICS}`}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_ANALYTICS}', { page_path: window.location.pathname });
+            `,
+          }}
+        />
+      </Head>
       <Layout key={router.asPath}>
         <motion.div
           initial='pageInitial'
