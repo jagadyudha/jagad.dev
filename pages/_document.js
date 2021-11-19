514c6840f3c498d5d2c2c74b440f354f42327228
Jagad Yudha Awali <41937681+jagadyudha@users.noreply.github.com>
2021-11-19 15:40:46 +0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head />
-        <body className='bg-mydark'>
+        <body className='bg-white'>
           <Main />
           <NextScript />
         </body>
