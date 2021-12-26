4ab0fa1e76c558a172f5122bfce8f7facc4602e4
unknown <jagadyudhaawali@gmail.com>
2021-12-26 12:57:14 +0700

    change 404 text

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,7 +5,7 @@ const ErrorCustomPage = () => {
     <div className='text-center justify-center items-center min-h-[55vh]'>
       <NextSeo title='404 - Jagad Yudha Awali' />
       <h1 className='text-4xl text-white font-bold'>404</h1>
-      <p className='text-lg text-white font-normal'>{`Awwww... don't cry.`}</p>
+      <p className='text-lg text-white font-normal'>{`h3h3h3h3 don't cry.`}</p>
     </div>
   );
 };
