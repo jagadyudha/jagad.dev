18d3bb2af43ebc42a33e063d16833bff79f40664
unknown <jagadyudhaawali@gmail.com>
2022-05-20 12:53:23 +0700

    test: ads unit

--- components/posts/adsense.tsx
+++ components/posts/adsense.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 
 const Adsense = () => {
   return (
-    <ins
-      className='adsbygoogle'
-      style={{
-        display: 'block',
-        margin: '6px !important',
-      }}
-      data-ad-client='ca-pub-1510507608200585'
-      data-ad-slot='5136962755'
-      data-ad-format='auto'
-      data-full-width-responsive='true'
-    />
+    <div className='my-10 mx-6'>
+      <ins
+        className='adsbygoogle'
+        style={{
+          display: 'block',
+          margin: '6px !important',
+        }}
+        data-ad-client='ca-pub-1510507608200585'
+        data-ad-slot='5136962755'
+        data-ad-format='auto'
+        data-full-width-responsive='true'
+      />
+    </div>
   );
 };
 
