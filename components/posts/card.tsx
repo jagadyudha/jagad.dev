19ad367d91da71e6187612809e66a2a2a69850a6
unknown <jagadyudhaawali@gmail.com>
2022-05-21 06:21:06 +0700

    fix: link on posts card

--- components/posts/card.tsx
+++ components/posts/card.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from '@/components/image';
 import Tags from '@/components/posts/tags';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export interface postProps {
   slug: string;
@@ -18,44 +19,46 @@ const PostCard: React.FC<postProps> = (props) => {
   const router = useRouter();
 
   return (
-    <a href={`/posts/${slug}`} key={slug} className='group'>
-      <div className='relative mx-auto h-56 max-w-3xl md:h-72 xl:h-80'>
-        <div className='absolute h-full w-full group-hover:opacity-70'>
-          <Image
-            className='rounded-md'
-            src={header}
-            layout='fill'
-            objectFit='cover'
-            alt={title}
-          />
+    <Link href={`/posts/${slug}`} key={slug}>
+      <a className='group'>
+        <div className='relative mx-auto h-56 max-w-3xl md:h-72 xl:h-80'>
+          <div className='absolute h-full w-full group-hover:opacity-70'>
+            <Image
+              className='rounded-md'
+              src={header}
+              layout='fill'
+              objectFit='cover'
+              alt={title}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className='relative '>
-        <p className='text-md'>
-          {`Posted on ${new Date(date).toLocaleString('default', {
-            month: 'long',
-          })} ${new Date(date).getDate()}, ${new Date(date).getFullYear()}`}
-        </p>
+        <div className='relative '>
+          <p className='text-md'>
+            {`Posted on ${new Date(date).toLocaleString('default', {
+              month: 'long',
+            })} ${new Date(date).getDate()}, ${new Date(date).getFullYear()}`}
+          </p>
 
-        <h3>{title} </h3>
+          <h3>{title} </h3>
 
-        <p className='text-gray-400'>{description}</p>
+          <p className='text-gray-400'>{description}</p>
 
-        <div>
-          {tags
-            .slice(0)
-            .reverse()
-            .map((tag, index) => (
-              <Tags
-                key={index}
-                name={tag}
-                href={`/posts?tag=${tag.toLowerCase()}`}
-              />
-            ))}
+          <div>
+            {tags
+              .slice(0)
+              .reverse()
+              .map((tag, index) => (
+                <Tags
+                  key={index}
+                  name={tag}
+                  href={`/posts?tag=${tag.toLowerCase()}`}
+                />
+              ))}
+          </div>
         </div>
-      </div>
-    </a>
+      </a>
+    </Link>
   );
 };
 
