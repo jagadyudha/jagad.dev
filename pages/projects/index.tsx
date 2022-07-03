910d0957ac9fa16d13c7b62db99007dc2d4a809c
unknown <jagadyudhaawali@gmail.com>
2022-07-03 13:46:50 +0700

    fix(index): replace fetcher with sanity api

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -3,7 +3,6 @@ import { NextSeo } from 'next-seo';
 import { InferGetStaticPropsType } from 'next';
 
 //lib
-import { cardTwitter } from '@/lib/seo';
 import { getContentIndex } from '@/lib/fetcher';
 
 //components
@@ -12,8 +11,24 @@ import ProjectCard from '@/components/projects/card';
 //data
 import DataSeo from '@/_data/seo.json';
 
+export type FrontmatterProps = {
+  title: string;
+  description: string;
+  date: Date;
+  stack: Array<string>;
+  header: string;
+};
+
+export type Props = {
+  frontmatter: FrontmatterProps;
+  content: string;
+  slug: {
+    current: string;
+  };
+};
+
 export async function getStaticProps() {
-  const projects = getContentIndex('projects');
+  const projects = await getContentIndex('projects');
   return {
     props: {
       projects,
@@ -45,20 +60,20 @@ const Projects = ({
       <div className='mx-auto my-5 md:my-10'>
         <div className='grid grid-cols-1 gap-5 md:gap-10'>
           {projects
-            .sort((a, b) => {
+            .sort((a: any, b: any) => {
               return (
                 new Date(b.frontmatter.date).valueOf() -
                 new Date(a.frontmatter.date).valueOf()
               );
             })
-            .map((item) => {
+            .map((item: Props) => {
               const { slug } = item;
               const { title, description, date, stack, header } =
                 item.frontmatter;
               return (
                 <ProjectCard
-                  key={slug}
-                  slug={slug}
+                  key={slug.current}
+                  slug={slug.current}
                   title={title}
                   description={description}
                   header={header}
