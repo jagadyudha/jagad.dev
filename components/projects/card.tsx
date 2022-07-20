6f571e895337bad8a5eb23620e4c7fe3f1c03430
unknown <jagadyudhaawali@gmail.com>
2022-07-20 23:21:56 +0700

    fix: handle translate for mobile devices

--- components/projects/card.tsx
+++ components/projects/card.tsx
@@ -18,7 +18,7 @@ const Card: React.FC<Props> = (props) => {
     <Link href={`/projects/${slug}`}>
       <a className='group mb-5 max-w-none flex-none items-center justify-between lg:flex lg:flex-row-reverse'>
         <div className='w-full overflow-hidden rounded-md bg-background_100 group-hover:bg-[#2C2D30] lg:ml-10'>
-          <div className=' translate-x-32'>
+          <div className='translate-x-16 lg:translate-x-32'>
             <Image
               className='rounded-sm'
               src={`/jagad.dev/projects/${slug}/header`}
@@ -30,7 +30,7 @@ const Card: React.FC<Props> = (props) => {
           </div>
         </div>
 
-        <div className='lg:w-[700px]'>
+        <div className='lg:w-[900px]'>
           <h3 className='group-hover:text-primary group-hover:underline'>
             {title}
           </h3>
