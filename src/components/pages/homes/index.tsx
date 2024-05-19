710118785c6e5ecd6065cd91c09a7e063243fed0
Jagad Yudha Awali <41937681+jagadyudha@users.noreply.github.com>
2024-05-19 15:47:20 +0700

    Update index.tsx

--- src/components/pages/homes/index.tsx
+++ src/components/pages/homes/index.tsx
@@ -20,7 +20,7 @@ const HomeIndex = ({ featuredPost, allTimeCode }: Props) => {
           {`Hey there! I'm\n`} <span className='text-primary'>Jagad Yudha</span>
         </h1>
         <p className='text-md mb-10 text-gray-400'>
-          {`I am a wizard of app enchantment who specializes in front-end spells. Over the course of my magical journey, I've summoned my text editor for a grand total of `}
+          {`A wizard of app enchantment who specializes in front-end spells. Over the course of my magical journey, I've summoned my text editor for a grand total of `}
           <span className=' font-bold text-white'>
             {allTimeCode?.data?.text}
           </span>
